c0b8ece6775e35de437efad16472f286b73717d6
anuradhakanse01 <50920536+anuradhakanse01@users.noreply.github.com>
2023-01-21 21:00:21 +0530

    Add files via upload

---
+++ week5/day1/CreateLead.java
@@ -0,0 +1,17 @@
+package week5.day1;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CreateLead extends BaseClass{
+
+	@Test
+	public void runCreateLead() {
+
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+	}
+}
